e0c285bd22550b2157098aa0c5dadbcc367a6442
Zachary Wasserman <zachwass2000@gmail.com>
2017-06-12 17:24:25 -0700

    Add ConfigPlugin helpers

---
+++ examples/config/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/kolide/osquery-golang"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf(`Usage: %s SOCKET_PATH\n
+
+Registers an example config plugin.
+
+Test with an invocation like:
+
+sudo ./example_call /var/osquery/osquery.em config example_config genConfig
+`, os.Args[0])
+		os.Exit(1)
+	}
+
+	serv, err := osquery.NewExtensionManagerServer("example_config", os.Args[1], 1*time.Second)
+	if err != nil {
+		fmt.Printf("Error creating extension: %v\n", err)
+		os.Exit(1)
+	}
+	serv.RegisterPlugin(osquery.NewConfigPlugin(&ExampleConfig{}))
+
+	// Shut down server when process killed so that we don't leave the unix
+	// domain socket file on the filesystem.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill, syscall.SIGTERM)
+	go func() {
+		<-sig
+		fmt.Println("Stopping extension server.")
+		err := serv.Shutdown()
+		if err != nil {
+			fmt.Println("Error shutting down server: " + err.Error())
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}()
+
+	fmt.Println("Starting extension server...")
+	err = serv.Start()
+	if err != nil {
+		fmt.Println("Error starting server: " + err.Error())
+		os.Exit(1)
+	}
+}
+
+type ExampleConfig struct{}
+
+func (f *ExampleConfig) Name() string {
+	return "example_config"
+}
+
+func (f *ExampleConfig) GenerateConfigs(ctx context.Context) (map[string]string, error) {
+	return map[string]string{
+		"config1": `
+{
+  "options": {
+    "host_identifier": "hostname",
+    "schedule_splay_percent": 10
+  },
+  "schedule": {
+    "macos_kextstat": {
+      "query": "SELECT * FROM kernel_extensions;",
+      "interval": 10
+    },
+    "foobar": {
+      "query": "SELECT foo, bar, pid FROM foobar_table;",
+      "interval": 600
+    }
+  }
+}
+`,
+	}, nil
+}
