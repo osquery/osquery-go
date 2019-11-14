6032425f781e4b2626177be773db354868d5a481
seph <seph@directionless.org>
2019-11-14 10:55:40 -0500

    Return options

--- plugin/table/table.go
+++ plugin/table/table.go
@@ -46,7 +46,7 @@ func (t *Plugin) Routes() osquery.ExtensionPluginResponse {
 			"id":   "column",
 			"name": col.Name,
 			"type": string(col.Type),
-			"op":   "0",
+			"op":   strconv.FormatUint(uint64(col.Options()), 10),
 		})
 	}
 	return routes
